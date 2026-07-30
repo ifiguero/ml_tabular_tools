b09854dca8ccb56477d814f64cee459f29677d99
Israel Figueroa <israel.figueroa@usm.cl>
2026-07-29 21:54:22 -0400

    upgrade

--- run_pregnancy.py
+++ run_pregnancy.py
@@ -1,108 +1,105 @@
 from loader import BasicLoader
 from trainer import BinaryTuner
 
-columns = ['ID',
- 'Class_GDM',
- 'Class_PTB',
- 'Class_Macrosomia',
- 'Age (years)',
+T3_pred_continuo = ['Age (years)',
  'BMI 1T (Kg/m2)',
  'BMI 2T (Kg/m2)',
  'BMI 3T (Kg/m2)',
  'Basal Glycaemia 1T (mg/dl)',
  'Basal Glycaemia 2T (mg/dl)',
  'Glycaemia 2h post 75 g (2T) (mg/dl)',
- 'TSH 1T (mIU/L)',
  'FT4 1T (ng/dl)',
- 'TT4 1T (ug/ml)',
- 'TT3 1T (ng/ml)',
- 'TSH 2T (mIU/L)',
  'FT4 2T (ng/dl)',
- 'TT4 2T (ug/ml)',
- 'TT3 2T (ng/ml)',
- 'TSH 3T (mIU/L)',
  'FT4 3T (ng/dl)',
- 'TT4 3T (ug/ml)',
- 'TT3 3T (ng/ml)',
- 'Gestational age at delivery (weeks)',
- 'Newborn weight (g)',
- 'Newborn height (cm)',
- 'Newborn sex (F=1, M=2)',
- 'Newborn TSH (mIU/L)',
- 'Newborn FT4 (ng/dl)',
- 'Newborn TT4 (ug/ml)',
- 'Newborn TT3 (ng/ml)']
-
-full_discrete=['Newborn sex (F=1, M=2)']
-
-pred_continuo = ['Age (years)',
- 'BMI 1T (Kg/m2)',
- 'BMI 2T (Kg/m2)',
- 'BMI 3T (Kg/m2)',
- 'Basal Glycaemia 1T (mg/dl)',
- 'Basal Glycaemia 2T (mg/dl)',
- 'Glycaemia 2h post 75 g (2T) (mg/dl)',
  'TSH 1T (mIU/L)',
- 'FT4 1T (ng/dl)',
- 'TT4 1T (ug/ml)',
- 'TT3 1T (ng/ml)',
  'TSH 2T (mIU/L)',
- 'FT4 2T (ng/dl)',
- 'TT4 2T (ug/ml)',
- 'TT3 2T (ng/ml)',
  'TSH 3T (mIU/L)',
- 'FT4 3T (ng/dl)',
+ 'TT4 1T (ug/ml)',
+ 'TT4 2T (ug/ml)',
  'TT4 3T (ug/ml)',
+ 'TT3 1T (ng/ml)',
+ 'TT3 2T (ng/ml)',
  'TT3 3T (ng/ml)']
 
-noT3_pred_continuo = ['Age (years)',
+T2_pred_continuo = ['Age (years)',
  'BMI 1T (Kg/m2)',
  'BMI 2T (Kg/m2)',
  'Basal Glycaemia 1T (mg/dl)',
  'Basal Glycaemia 2T (mg/dl)',
  'Glycaemia 2h post 75 g (2T) (mg/dl)',
- 'TSH 1T (mIU/L)',
  'FT4 1T (ng/dl)',
- 'TT4 1T (ug/ml)',
- 'TT3 1T (ng/ml)',
- 'TSH 2T (mIU/L)',
  'FT4 2T (ng/dl)',
+ 'TSH 1T (mIU/L)',
+ 'TSH 2T (mIU/L)',
+ 'TT4 1T (ug/ml)',
  'TT4 2T (ug/ml)',
+ 'TT3 1T (ng/ml)',
  'TT3 2T (ng/ml)']
 
 
+T1_pred_continuo = ['Age (years)',
+ 'BMI 1T (Kg/m2)',
+ 'Basal Glycaemia 1T (mg/dl)',
+ 'FT4 1T (ng/dl)',
+ 'TSH 1T (mIU/L)',
+ 'TT4 1T (ug/ml)',
+ 'TT3 1T (ng/ml)']
+
 PTB_noGDM = BasicLoader('Dataset_Pregnancy_Outcomes_binary.csv',
     target=['Class_PTB'],
-    continuous=pred_continuo, discrete=full_discrete,
+    continuous=T3_pred_continuo,
     exclude={"Class_GDM": [1], })
-PTB_noGDM.set_label('Class_PTB', 'PTB noGDM')
+PTB_noGDM.set_label('Class_PTB', 'PTB noGDM T3')
 
 PTB_GDM = BasicLoader('Dataset_Pregnancy_Outcomes_binary.csv',
     target=['Class_PTB'],
-    continuous=pred_continuo, discrete=full_discrete,
+    continuous=T3_pred_continuo,
     exclude={"Class_GDM": [0], })
-PTB_GDM.set_label('Class_PTB', 'PTB GDM')
+PTB_GDM.set_label('Class_PTB', 'PTB GDM T3')
 
 NBM_GDM = BasicLoader('Dataset_Pregnancy_Outcomes_binary.csv',
     target=['Class_Macrosomia'],
-    continuous=noT3_pred_continuo, discrete=full_discrete,
+    continuous=T2_pred_continuo,
     exclude={"Class_GDM": [0], })
-NBM_GDM.set_label('Class_Macrosomia', 'NBM GDM')
+NBM_GDM.set_label('Class_Macrosomia', 'NBM GDM T2')
 
-PTB_complete = BasicLoader('Dataset_Pregnancy_Outcomes_binary.csv',
+PTB_T3 = BasicLoader('Dataset_Pregnancy_Outcomes_binary.csv',
     target=['Class_PTB'],
-    continuous=pred_continuo, discrete=full_discrete)
-PTB_complete.set_label('Class_PTB', 'PTB Complete')
+    continuous=T3_pred_continuo)
+PTB_T3.set_label('Class_PTB', 'PTB T3')
 
-NMB_complete = BasicLoader('Dataset_Pregnancy_Outcomes_binary.csv',
+NMB_T3 = BasicLoader('Dataset_Pregnancy_Outcomes_binary.csv',
     target=['Class_Macrosomia'],
-    continuous=noT3_pred_continuo, discrete=full_discrete)
-NMB_complete.set_label('Class_Macrosomia', 'NBM Complete')
+    continuous=T3_pred_continuo)
+NMB_T3.set_label('Class_Macrosomia', 'NBM T3')
 
-studies = [ ('PTB noGDM', PTB_noGDM), ('PTB GDM', PTB_GDM), ('NBM GDM', NBM_GDM), ('PTB Complete', PTB_complete), ('NBM Complete', NMB_complete) ]
+
+PTB_T2 = BasicLoader('Dataset_Pregnancy_Outcomes_binary.csv',
+    target=['Class_PTB'],
+    continuous=T2_pred_continuo)
+PTB_T2.set_label('Class_PTB', 'PTB T2')
+
+NMB_T2 = BasicLoader('Dataset_Pregnancy_Outcomes_binary.csv',
+    target=['Class_Macrosomia'],
+    continuous=T2_pred_continuo)
+NMB_T2.set_label('Class_Macrosomia', 'NBM T2')
+
+
+PTB_T1 = BasicLoader('Dataset_Pregnancy_Outcomes_binary.csv',
+    target=['Class_PTB'],
+    continuous=T1_pred_continuo)
+PTB_T1.set_label('Class_PTB', 'PTB T1')
+
+NMB_T1 = BasicLoader('Dataset_Pregnancy_Outcomes_binary.csv',
+    target=['Class_Macrosomia'],
+    continuous=T1_pred_continuo)
+NMB_T1.set_label('Class_Macrosomia', 'NBM T1')
+
+
+studies = [ ('PTB noGDM T3', PTB_noGDM), ('PTB GDM T3', PTB_GDM), ('NBM GDM T2', NBM_GDM), ('PTB T3', PTB_T3), ('NBM T3', NMB_T3), ('PTB T2', PTB_T2), ('NBM T2', NMB_T2), ('PTB T1', PTB_T1), ('NBM T1', NMB_T1) ]
 
 for target, study in studies:
     study_df = study.get_dataset(target)
-    trial = BinaryTuner(study_df, target, n_seeds=15, tuneScoring='roc_auc')
+    trial = BinaryTuner(study_df, target, n_seeds=25, tuneScoring='roc_auc')
     trial.fit()
     trial.wrap_and_save()
